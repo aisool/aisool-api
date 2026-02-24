95cb1a7d7e5c7a0961404cd259b8aad6a61c26b1
aisool <contact@aisool.com>
2026-02-24 07:13:38 +0100

    Update NodeJs.js

--- src/via-endpoint/NodeJs.js
+++ src/via-endpoint/NodeJs.js
@@ -28,3 +28,34 @@ const callAi = async () => {
     console.error(err);
   }
 };
+
+
+// image generation
+
+const axios = require('axios');
+
+const generateArt = async () => {
+  try {
+    const res = await axios.post(
+      'https://api.aisool.com/v1/images/generations',
+      {
+        prompt: 'Oil painting of a space station',
+        n: 1,
+        size: '1024x1024'
+      },
+      {
+        headers: {
+          'Authorization': 'Bearer ${safeKey}', ${warning}
+          'Content-Type': 'application/json'
+        } 
+      }
+    );
+
+    const data = res.data;
+    console.log('Image Source:', data.data[0].url);
+    console.log('Timestamp:', data.created);
+
+  } catch (err) {
+    console.error(err);
+  }
+};
